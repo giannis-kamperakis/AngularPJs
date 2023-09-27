bebdd749d46af7fbbe087fb47ef940ba1b11cd9d
Giannis Kamperakis <giannis.kamperakis@braincube.gr>
2023-09-27 12:25:49 +0300

    Section 6 completed

--- TrainingProject/src/app/app.component.ts
+++ TrainingProject/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
   title = 'TrainingProject';
+  parentMessage:string = "Message coing from the parent component!!";
+  messageFromChild:string = "";
+
+  emittedMessageFromChild: string = "";
+
+  @ViewChild(PostComponent) childComp: any;
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    this.messageFromChild = this.childComp.childMessage;
+  }
+
+  receiveMessage($event:any){
+    this.emittedMessageFromChild = $event;
+  }
+
 }
--- TrainingProject/src/app/post/post.component.ts
+++ TrainingProject/src/app/post/post.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  title = "List of Posts"
+  postList = "This is a message from the parent to the postList component!!!@@@!!"
+
+  childMessage = "This is a message from your child!"
+  
+  @Input() fromParent:string | undefined;
+
+  outputChildMessage = "Emitted message from your child! How dare you leave m.....!"
+  @Output() messageEvent = new EventEmitter<string>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  sendMessage(){
+    this.messageEvent.emit(this.outputChildMessage);
+  }
 
 }
