806939d52678cd9328d7a21bbdf18d5a1bf169c3
Giannis Kamperakis <giannis.kamperakis@braincube.gr>
2023-10-04 15:16:21 +0300

    Pipes, custom pipes

--- Directives/src/app/app.module.ts
+++ Directives/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppendPipe } from './Pipes/append.pipe';
+import { AppendCLIPipe } from './Pipes/append-cli.pipe';
+import { SummaryPipe } from './Pipes/summary.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppendPipe,
+    AppendCLIPipe,
+    SummaryPipe
+
   ],
   imports: [
     BrowserModule,
